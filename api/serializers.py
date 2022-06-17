541a3a027caa91931c9f11b2c26a66461d151b05
Alberto <allanos@sol4it.net>
2022-06-17 14:56:27 -0500

    Almost Complete

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,20 @@
 from rest_framework import serializers
+from api.models import Team
+from api.models import Player
+from api.models import Staff
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+          model = Team
+          fields = '__all__'
 
-class HelloSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=10)
+class PlayerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+class StaffSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Staff
+        fields = '__all__'
